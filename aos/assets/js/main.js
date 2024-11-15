26c0cb78c42c68c1e3403c904afd465f184a24e8
Naiem-Devs <dnsnaiem@gmail.com>
2024-11-15 20:16:45 +0600

    first commit

---
+++ aos/assets/js/main.js
@@ -0,0 +1,48 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+  $('.siteBar-btn').click( function (){ 
+    $('.mobile-menu').toggleClass('siteBar');   
+  }); 
+
+
+  // owlCarousel
+  $(".hero_slider").owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 1,
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      767: {
+        items: 1
+      },
+      992: {
+        items: 1
+      }
+    }
+  });
+
+  $(document).ready(function() {
+    $('#Video').magnificPopup({
+        type: 'iframe'
+    });
+});
+ 
+document.querySelector('html').classList.remove('no-js');
+    if (!window.Cypress) {
+
+      AOS.init({
+        mirror: true,
+        duration: 1500,
+        initClassName: 'aos-init',
+        once: true,
+      });
+
+    }
+
+})(jQuery);
